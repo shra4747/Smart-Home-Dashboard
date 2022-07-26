f8e999e0504c12c0277ed4da6c18ea34bce1a0c6
shra4747 <shra4747@gmail.com>
2022-07-26 01:07:58 -0400

    Scenes in Development, removed govee_api, changed port to 8080

--- src/main/main.py
+++ src/main/main.py
@@ -6,6 +6,14 @@ import kasa_func
 app = Flask(__name__)
 
 
+govee_devices = {'room1': {'mac': '0F:21:A4:C1:38:16:C9:C2', 'model': 'H6110'},
+                 'room2': {'mac': '5F:EB:A4:C1:38:00:92:D2', 'model': 'H6159'},
+                 'desk': {'mac': 'AB:B7:A4:C1:38:BE:1C:6D', 'model': 'H6159'}}
+
+
+kasa_devices = {'fan': '10.0.0.207', 'lamp': '10.0.0.27'}
+
+
 @app.route('/govee/toggle/<state>/<mac>&<model>')
 def toggle(state, mac, model):
     return govee_func.toggle(state, mac, model)
@@ -21,5 +29,31 @@ def kasa_toggle(state, ip):
     return kasa_func.toggle(state, ip)
 
 
+@app.route('/scenes/<scene>')
+def scenes(scene):
+    if scene == "all_on":
+        govee_func.toggle(
+            "on", govee_devices['room1']['mac'], govee_devices['room1']['model'])
+        govee_func.toggle(
+            "on", govee_devices['room2']['mac'], govee_devices['room2']['model'])
+        govee_func.toggle(
+            "on", govee_devices['desk']['mac'], govee_devices['desk']['model'])
+
+        kasa_func.toggle('on', kasa_devices['lamp'])
+
+        return '200'
+    elif scene == "all_off":
+        govee_func.toggle(
+            "off", govee_devices['room1']['mac'], govee_devices['room1']['model'])
+        govee_func.toggle(
+            "off", govee_devices['room2']['mac'], govee_devices['room2']['model'])
+        govee_func.toggle(
+            "off", govee_devices['desk']['mac'], govee_devices['desk']['model'])
+
+        kasa_func.toggle('off', kasa_devices['lamp'])
+
+        return '200'
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='80')
+    app.run(debug=False, host='0.0.0.0', port='8080')
